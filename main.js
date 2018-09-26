c24bbe6ae715bd82379f128ce22732b7d28facb7
TylereDupont <tyleredupont@gmail.com>
2018-09-25 18:19:14 -0700

    Ch4
    Added radio buttons to cost calc

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ function calc()
 	var numPeople = document.getElementById('numPeople').value;
 	var numDays = document.getElementById('numDays').value;
 	
-	//This formula Estimates as $1200 plane ticket per person and $200 per person per day plus $200 a day for a hotel.
-	document.getElementById('result').innerHTML = "Your estimated cost of travel is $" + ((numPeople * 1200) + (numDays * numPeople * 200) + 200); 
+	var peopleImg = [document.getElementById('person1'), 	document.getElementById('person2'), document.getElementById('person3')];
+	
+	//Prepare Food Budget multiplier
+	var foodMultiplier  = 0;
+	if (document.getElementById('foodBudget1').checked)
+		{
+			foodMultiplier = 1;
+		}
+	if (document.getElementById('foodBudget2').checked)
+		{
+			foodMultiplier = 2;
+		}
+	if (document.getElementById('foodBudget3').checked)
+		{
+			foodMultiplier = 4;
+		}
+	//This formula Estimates as $1200 plane ticket per person and $200 per person per day plus $200 a day for a hotel plus food costs.
+	document.getElementById('result').innerHTML = "Your estimated cost of travel is $" + ((numPeople * 1200)+(numDays * numPeople * 200)+(200 * numDays)+(numPeople * foodMultiplier * 50)); 
+	
+	
+	//This section is for displaying the people images correctly
+	//Declare Variables
+	
 }

34adb56f8f64b073190f0c13aced32bc7dced5ea
TylereDupont <tyleredupont@gmail.com>
2018-11-13 10:10:20 -0700

    ch9

--- assignments/cis166aa/wk9.js
+++ assignments/cis166aa/wk9.js
@@ -1,15 +1,16 @@
-//Gather neccessary variables
-	//Inputs
-var nameInput = document.getElementById("name").value;
-var emailInput = document.getElementById("email").value;
-var messageInput = document.getElementById("message").value;
-	//Outputs
-var nameOutput = document.getElementById("name-output");
-var emailOutput = document.getElementById("email-output");
-var messageOutput = document.getElementById("message-output");
 
 //Main function to be ran by form button
 function convert(){
+	//Gather neccessary variables
+	//Inputs
+	var nameInput = document.getElementById("name").value;
+	var emailInput = document.getElementById("email").value;
+	var messageInput = document.getElementById("message").value;
+	//Outputs
+	var nameOutput = document.getElementById("name-output");
+	var emailOutput = document.getElementById("email-output");
+	var messageOutput = document.getElementById("message-output");
+
 	//Take input from the name/email/message Inputs and use .replace to remove any unnecessary chracters
 	nameOutput.innerHTML = nameInput.replace(/\W/g, '');
 	emailOutput.innerHTML = emailInput;
